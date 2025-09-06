cb469354da62827c6bce6b079c0e247fce9e0aaf
pedrofoschiani <pedro.foschiani@outlook.com>
2025-09-06 09:34:24 -0300

    vamos ver se a coleta de icons funciona agora

--- android/src/main/java/com/blome/applist/AppListPlugin.java
+++ android/src/main/java/com/blome/applist/AppListPlugin.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Base64;
+import java.io.ByteArrayOutputStream;
 
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
@@ -19,29 +25,30 @@ public class AppListPlugin extends Plugin {
 
     @PluginMethod
     public void getInstalledApps(PluginCall call) {
-        String myPackageName = getContext().getPackageName();
         PackageManager pm = getContext().getPackageManager();
         JSArray result = new JSArray();
-
         List<PackageInfo> installedPackages = pm.getInstalledPackages(PackageManager.GET_META_DATA);
 
         for (PackageInfo pi : installedPackages) {
             ApplicationInfo appInfo = pi.applicationInfo;
-            if (appInfo == null) {
-                continue;
-            }
-
-            Intent launchIntent = pm.getLaunchIntentForPackage(appInfo.packageName);
-            boolean isMyApp = myPackageName.equals(appInfo.packageName);
-
 
             boolean isSystemApp = (appInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
             boolean isUpdatedSystemApp = (appInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0;
+            Intent launchIntent = pm.getLaunchIntentForPackage(appInfo.packageName);
+            boolean isMyApp = getContext().getPackageName().equals(appInfo.packageName);
 
             if (launchIntent != null && !isMyApp && (!isSystemApp || isUpdatedSystemApp)) {
                 JSObject app = new JSObject();
                 app.put("name", appInfo.loadLabel(pm).toString());
                 app.put("packageName", appInfo.packageName);
+
+                try {
+                    Drawable iconDrawable = appInfo.loadIcon(pm);
+                    String encodedIcon = drawableToBase64(iconDrawable);
+                    app.put("icon", encodedIcon);
+                } catch (Exception e) {
+                }
+
                 result.put(app);
             }
         }
@@ -49,5 +56,26 @@ public class AppListPlugin extends Plugin {
         JSObject ret = new JSObject();
         ret.put("apps", result);
         call.resolve(ret);
+    }
+
+    private String drawableToBase64(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        int width = drawable.getIntrinsicWidth();
+        width = width > 0 ? width : 96;
+        int height = drawable.getIntrinsicHeight();
+        height = height > 0 ? height : 96;
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        byte[] byteArray = outputStream.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.NO_WRAP);
     }
 }
