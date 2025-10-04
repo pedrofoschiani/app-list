677d0ad2da233fd0cf7c8870aae4030038e1fc5c
pedrofoschiani <pedro.foschiani@outlook.com>
2025-10-04 08:08:40 -0300

    vamo la

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,14 +1,14 @@
-export interface AppListPlugin {
-  getInstalledApps(): Promise<{ apps: AppInfo[] }>;
-}
-
 export interface AppInfo {
   name: string;
   packageName: string;
 }
 
+export interface AppListPlugin {
+  getInstalledApps(): Promise<{ apps: AppInfo[] }>;
+}
+
 export interface BlockAppsPlugin {
-  setBlockedApps(options: { packages: string[] }): Promise<void>;
+  setBlockedPackages(options: { packages: string[] }): Promise<void>;
 }
 
 export interface BlomePlugin extends AppListPlugin, BlockAppsPlugin {}
--- src/web.ts
+++ src/web.ts
@@ -2,6 +2,10 @@ import { WebPlugin } from '@capacitor/core';
 import type { AppInfo, BlomePlugin } from './definitions';
 
 export class BlomeWeb extends WebPlugin implements BlomePlugin {
+  
+  async setBlockedPackages(_options: { packages: string[]; }): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
 
   async setBlockedApps(_options: { packages: string[]; }): Promise<void> {
     console.warn('O método setBlockedPackages não é compatível com a web.');
