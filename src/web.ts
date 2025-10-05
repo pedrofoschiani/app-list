ad688ac15ee7763bcdaed32f25c994a1719972c2
pedrofoschiani <pedro.foschiani@outlook.com>
2025-10-05 09:45:39 -0300

    mudança no web.ts e na versão do plugin

--- src/web.ts
+++ src/web.ts
@@ -1,5 +1,5 @@
 import { WebPlugin, registerPlugin } from '@capacitor/core';
-import type { AppInfo, AppListPlugin, BlockAppsPLugin} from './definitions';
+import type { AppInfo, AppListPlugin, BlockAppsPlugin} from './definitions';
 
 export class AppListWeb extends WebPlugin implements AppListPlugin {
   async getInstalledApps(): Promise<{ apps: AppInfo[] }> {
@@ -11,12 +11,11 @@ export const AppList = registerPlugin<AppListPlugin>('AppList', {
   web: () => import('./web').then(m => new m.AppListWeb()),
 });
 
-export class BlockAppsWeb extends WebPlugin implements BlockAppsPLugin {
-  async setBlockedApps(_options: { packageNames: string[] }): Promise<void> {
-    console.warn('O plugin BlockApps não é compatível com a plataforma web.');
-    return;
+export class BlockAppsWeb extends WebPlugin implements BlockAppsPlugin {
+  async setBlockedPackages(_options: { packages: string[]; }): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
-export const BlockApps = registerPlugin<BlockAppsPLugin>('BlockApps', {
+export const BlockApps = registerPlugin<BlockAppsPlugin>('BlockApps', {
   web: () => import('./web').then(m => new m.BlockAppsWeb()),
 });
