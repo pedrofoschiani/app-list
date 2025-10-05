08e6426416f5ad5e9b0fed543e7638feb8e57c24
pedrofoschiani <pedro.foschiani@outlook.com>
2025-10-05 09:03:06 -0300

    iindex

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export * from './definitions';
+import { registerPlugin } from '@capacitor/core';
+import type { AppListPlugin } from './definitions';
+
+const AppList = registerPlugin<AppListPlugin>('AppList');
+
+export { AppListPlugin, AppInfo } from './definitions';
+export { AppList };
+
+import type { BlockAppsPlugin } from './definitions';
+
+const BlockApps = registerPlugin<BlockAppsPlugin>('BlockApps');
+
+export { BlockAppsPlugin} from './definitions';
+export { BlockApps };
